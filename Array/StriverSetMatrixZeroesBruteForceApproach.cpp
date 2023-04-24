1a3fc9231df64698760ce30bfa66d70bdb512411
Parul2004 <parul1007.be21@chitkara.edu.in>
2023-04-23 23:55:24 -0700

    initial commit

--- Array/StriverSetMatrixZeroesBruteForceApproach.cpp
+++ Array/StriverSetMatrixZeroesBruteForceApproach.cpp
@@ -1,8 +1,67 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
+void rowMatrix(vector<vector<int>> &matrix, int i, int n, int m)
+{
+    for (int j = 0; j < m; j++)
+    {
+        if (matrix[i][j] != 0)
+        {
+            matrix[i][j] = -1;
+        }
+    }
+}
+void colMatrix(vector<vector<int>> &matrix, int j, int n, int m)
+{
+    for (int i = 0; i < n; i++)
+    {
+        if (matrix[i][j] != 0)
+        {
+            matrix[i][j] = -1;
+        }
+    }
+}
+vector<vector<int>> setMatrixZeroes(vector<vector<int>> &matrix, int n, int m)
+{
 
-int main(){
-    vector<vector<int>>v = {{0,1,2,0},{3,4,5,2},{1,3,1,5}};
-    
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (matrix[i][j] == 0)
+            {
+                rowMatrix(matrix, i, n, m);
+
+                colMatrix(matrix, j, n, m);
+            }
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (matrix[i][j] == -1)
+            {
+                matrix[i][j] = 0;
+            }
+        }
+    }
+    return matrix;
+}
+int main()
+{
+    vector<vector<int>> v = {{0, 1, 2, 0}, {3, 4, 5, 2}, {1, 3, 1, 5}};
+    int n = v.size();
+    int m = v[0].size();
+    setMatrixZeroes(v, n, m);
+
+    for (auto i : v)
+    {
+        for (auto j : i)
+        {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
     return 0;
 }
